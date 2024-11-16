6f7d01064ebc01a95fb0419646b0460b148839b5
Everson Henrich da Silva <eversonhs.es@gmail.com>
2024-11-16 18:52:22 -0300

    Ajustes no comando de execução do submit job do spark

--- services/run_all.ps1
+++ services/run_all.ps1
@@ -1,10 +1,7 @@
-$rw_tr_scripts='rw_tr_vacinacao_covid_19.py'
-$tr_rf_dm_scripts='tr_rf_dm_campanhas.py', 'tr_rf_dm_estabelecimentos.py', 'tr_rf_dm_vacinas.py', 'tr_rf_dm_municipios.py', 'tr_rf_dm_pacientes.py'
-$tr_rf_ft_scripts='tr_rf_ft_vacinacao.py'
-$scripts=$rw_tr_scripts, $tr_rf_dm_scripts, $tr_rf_ft_scripts
+$scripts='rw_tr_vacinacao_covid_19.py', 'tr_rf_dm_campanhas.py', 'tr_rf_dm_estabelecimentos.py', 'tr_rf_dm_vacinas.py', 'tr_rf_dm_municipios.py', 'tr_rf_dm_pacientes.py', 'tr_rf_ft_vacinacao.py'
 
 foreach ($script in $scripts)  {
-    docker exec spark-client bash -c 'spark-submit --master $SPARK_MASTER_URI --jars $MYSQL_CONNECTORJ_PATH --driver-class-path $MYSQL_CONNECTORJ_PATH --executor-memory 1g --num-executors 2 --executor-cores 1 ./jobs/${1}' -f $script
+    docker exec spark-client bash -c 'spark-submit --master $SPARK_MASTER_URI --jars $POSTGRESQL_JDBC_DRIVER --driver-class-path $POSTGRESQL_JDBC_DRIVER --executor-memory 8g --executor-cores 4 ./jobs/bulk/${1}' -f $script
 
 }
 
